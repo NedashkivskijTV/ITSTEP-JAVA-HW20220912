d254b1ecad1397f8fd3a165e1b8208e01a66881d
NedashkivskijTV <nedashkivskijtv@ukr.net>
2022-09-13 15:29:20 +0300

    add homework from 12.09.2022

--- HW20220912/src/edu/itstep/hw20220912/MyListInt.java
+++
@@ -1,9 +0,0 @@
-package edu.itstep.hw20220912;
-
-public interface MyListInt {
-    void addToHead(int val);
-    void addToTail(int val);
-    void deleteFromHead();
-    void deleteFromTail();
-    void showList();
-}
